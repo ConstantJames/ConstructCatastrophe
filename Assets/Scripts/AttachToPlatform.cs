a1cc1a4cb7878682fb8c0c36041f01cfad733712
binauhank <binauhankristian@gmail.com>
2025-02-26 21:43:39 -0800

    Reworked movement + added object detection

--- Assets/Scripts/AttachToPlatform.cs
+++ Assets/Scripts/AttachToPlatform.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class AttachToPlatform : MonoBehaviour
 {
+    /* Will use this script to automatically set anything inside the Build Area (most likely through a trigger) 
+     * as a child of the platform/shaker table, so they get affected by rotations/movements that happen to the table. */
+
     void OnTriggerEnter(Collider other)
     {
-        other.transform.SetParent(transform); // can set this under an if statement
+        other.transform.SetParent(transform); // can set this under an if statement with CompareTag
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,26 +7,73 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rbPlayer;
     private Vector3 direction;
+    private LayerMask pickableLayerMask;
 
-    private float speed = 5.0f;
+    public float speed = 10.0f;
+    private float horizontalVelocity;
+    private float verticalVelocity;
+
+    public float jumpForce = 10.0f;
+    private bool spaceDown = false;
+    private bool isJumping = false;
+    private bool isGrounded;
 
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
+        pickableLayerMask = LayerMask.GetMask("Pickable"); // SET EVERY OBJECT THAT CAN BE PICKED UP UNDER "Pickable" LAYER
     }
 
     void Update()
     {
-        float horizontalVelocity = Input.GetAxis("Horizontal");
-        float verticalVelocity = Input.GetAxis("Vertical");
+        // Movement + Jumping
+        horizontalVelocity = Input.GetAxis("Horizontal");
+        verticalVelocity = Input.GetAxis("Vertical");
 
         direction = new Vector3(horizontalVelocity, 0, verticalVelocity).normalized;
+
+        spaceDown = Input.GetButtonDown("Jump");
+
+        if (spaceDown)
+        {
+            isJumping = true;
+        }
+
+        // Player faces the direction they are moving in
+        Vector3 lookDirection = direction + gameObject.transform.position;
+        gameObject.transform.LookAt(lookDirection);
     }
 
     void FixedUpdate()
     {
-        rbPlayer.velocity = direction * speed;
+        // Movement + Jumping
+        rbPlayer.velocity = new Vector3(direction.x * speed, rbPlayer.velocity.y, direction.z * speed);
 
-        // rbPlayer.AddForce(direction * speed, forceMode);
+        if (isJumping && isGrounded)
+        {
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            isJumping = false;
+        }
+
+        // Detects if an object is in front of the player
+        Vector3 fwd = transform.TransformDirection(Vector3.forward) + transform.up * -0.5f;
+
+        Debug.DrawRay(transform.position, fwd * 10, Color.yellow); // Visible raycast in Scene view during play
+
+        if (Physics.Raycast(transform.position, fwd, 10, pickableLayerMask))
+        {
+            Debug.Log("Object detected");
+
+            // Pick up script goes here
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
